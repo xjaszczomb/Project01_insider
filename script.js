4df7c940d8bb11fffc01127c7ef1e78fa389d038
xjaszczomb <kovvalska@onet.pl>
2022-02-28 16:03:55 +0100

    Removing unnecessary code lines

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
-function widthChange() {
-    $('#pabout').css("width",$('#habout').width());
-}
-
 $(document).ready(()=> $('#pabout').css("width",$('#habout').width()) );
 $(window).resize(()=> $('#pabout').css("width",$('#habout').width()) );
