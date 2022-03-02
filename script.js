d3b0283a3223f241325b98b5522100b58fd753b9
xjaszczomb <kovvalska@onet.pl>
2022-03-02 15:55:42 +0100

    init dynamic js (wip)

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 $(document).ready(()=> $('#pabout').css("width",$('#habout').width()) );
 $(window).resize(()=> $('#pabout').css("width",$('#habout').width()) );
+
+
+const navbar = document.createElement("nav");
+const counter = document.createElement("div");
+counter.classList.add("page-counter","top","flex");
+
+counter.innerHTML = "<div class='page-counter-holder flex'>"+
+                        "<div class='page-nr'>1</div>"+
+                        "<hr class='page-line'>"+
+                        "<div class='page-nr'>4</div>"+
+                    "</div>";
+navbar.innerHTML ="<div class='navbar flex'>"+
+                    "<div class='nav-toggle'>"+
+                        "<img src='icons/menu.png' alt='menu'>"+
+                    "</div>"+
+                "</div>";
+$(".main-page").prepend(navbar,counter);
+
