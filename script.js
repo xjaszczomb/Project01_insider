2f76dc1b1cb51beaac27a8c1d57789864d321721
xjaszczomb <kovvalska@onet.pl>
2022-03-16 14:18:46 +0100

    Remove unnecessary features and simplify code

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function currentWidth() {
     return $(window).width();
 }
-function validNavbarAddress() {
-    return currentWidth()<992 ? "items/navbarMobile.html" : "items/navbarDesktop.html"
-}
 
 //navbar
 
@@ -11,20 +8,15 @@ const navbar = document.createElement("nav");
 const counter = document.createElement("div");
 counter.classList.add("page-counter","top","flex","w-100","h-100");
 
-function getNavbar() {
-    fetch(validNavbarAddress())
-        .then((response) => {
-            return response.text();
-        })
-        .then((data) => {
-            $("nav").empty();
-            navbar.innerHTML=data;
-            $(".main-page").prepend(navbar);
-        });
-}
-
-$(window).resize(()=> getNavbar());
-$(document).ready(()=> getNavbar());
+fetch("items/navbar.html")
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        $("nav").empty();
+        navbar.innerHTML=data;
+        $(".main-page").prepend(navbar);
+    });
 
 //counter
 
