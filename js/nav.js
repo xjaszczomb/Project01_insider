ad75372af9bb5905e19639fcd1f0814fb660777b
xjaszczomb <kovvalska@onet.pl>
2022-03-21 19:17:45 +0100

    New concept (less complicated) - navbar and page counter

--- js/nav.js
+++ js/nav.js
@@ -4,8 +4,6 @@ $(".nav-close").click(()=> {
         // $(".nav").removeClass("navbar-nav");
         $(".nav").attr("data-visible",false);
         $(".navbar").attr("aria-expanded",false);
-        $(".header-aboutus-holder").css("margin-block-start","0");
-        $(".header-holder").css("margin-block-start","0");
         
         $(".nav-toggle").attr("data-visible",true);
         $(".nav-close").attr("data-visible",false);
@@ -17,10 +15,7 @@ $(".nav-toggle").click(()=> {
     if(visible==="false") {
         // $(".nav").addClass("navbar-nav");
         $(".nav").attr("data-visible",true);
-        
         $(".navbar").attr("aria-expanded",true);
-        $(".header-aboutus-holder").css("margin-block-start","6em");
-        $(".header-holder").css("margin-block-start","6em");
 
         $(".nav-toggle").attr("data-visible",false);
         $(".nav-close").attr("data-visible",true);
--- js/script.js
+++ js/script.js
@@ -1,47 +1,7 @@
 function currentWidth() {
     return $(window).width();
 }
-
-//navbar
-
-const navbar = document.createElement("nav");
-const counter = document.createElement("div");
-counter.classList.add("page-counter","top","flex","w-100","h-100");
-
-// fetch("items/navbar.html")
-//     .then((response) => {
-//         return response.text();
-//     })
-//     .then((data) => {
-//         $("nav").empty();
-//         navbar.innerHTML=data;
-//         $(".main-page").prepend(navbar);
-//     });
-
-let xmlhttp= new XMLHttpRequest();
-xmlhttp.open("GET","items/navbar.html",false);
-xmlhttp.send(null);
-navbar.innerHTML=xmlhttp.responseText;
-$(".main-page").prepend(navbar);
-
-// jQuery.get('items/navbar.html', function(data) {
-//     return data;
-// });
-
-
-//counter
-
-fetch("items/counter.html")
-    .then((response) => {
-        return response.text();
-     })
-    .then((data) => {
-        counter.innerHTML=data;
-        $(".main-page").prepend(counter);
-    });
-
 //newsletter class adding/removing (resize)
-    
 $(document).ready(()=> { currentWidth()<992 ? $("#emailboxoverflow").removeClass("email-box-overflow") : $("#emailboxoverflow").addClass("email-box-overflow");});
 $(window).resize(()=> { currentWidth()<992 ? $("#emailboxoverflow").removeClass("email-box-overflow") : $("#emailboxoverflow").addClass("email-box-overflow");});
 
@@ -49,3 +9,55 @@ $(window).resize(()=> { currentWidth()<992 ? $("#emailboxoverflow").removeClass(
 $(document).ready(()=> $('#pabout').css("width",$('#habout').width()) );
 $(window).resize(()=> $('#pabout').css("width",$('#habout').width()) );
 $(document).scroll(()=> $('#pabout').css("width",$('#habout').width()) );
+
+
+
+
+
+
+
+
+
+//**********************************************************
+//               CONCEPT REQUIRES REACT 
+//**********************************************************
+
+// //navbar
+
+// const navbar = document.createElement("nav");
+// const counter = document.createElement("div");
+// counter.classList.add("page-counter","top","flex","w-100","h-100");
+
+// // fetch("items/navbar.html")
+// //     .then((response) => {
+// //         return response.text();
+// //     })
+// //     .then((data) => {
+// //         $("nav").empty();
+// //         navbar.innerHTML=data;
+// //         $(".main-page").prepend(navbar);
+// //     });
+
+// let xmlhttp= new XMLHttpRequest();
+// xmlhttp.open("GET","items/navbar.html",false);
+// xmlhttp.send(null);
+// navbar.innerHTML=xmlhttp.responseText;
+// $(".main-page").prepend(navbar);
+
+// // jQuery.get('items/navbar.html', function(data) {
+// //     return data;
+// // });
+
+// //counter
+
+// fetch("items/counter.html")
+//     .then((response) => {
+//         return response.text();
+//      })
+//     .then((data) => {
+//         counter.innerHTML=data;
+//         $(".main-page").prepend(counter);
+//     });
+
+// **********************************************************
+
