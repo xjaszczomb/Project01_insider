ff725337bae0ed2e1ba0702fce969dd26516dccb
xjaszczomb <kovvalska@onet.pl>
2022-03-29 17:23:23 +0200

    Email box: jquery animations, adding css transition and result, adding new icon for button

--- js/email.js
+++ js/email.js
@@ -7,27 +7,49 @@ function currentWidth() {
     return $(window).width();
 }
 function validate() {
-    let result;
-    if(currentWidth()<992) result=$('.result').first();
-    else result=$('.result').last();
+    let result, success;
+    if(currentWidth() < 992) result = $('.result').first();
+    else result = $('.result').last();
 
     const email = $('.email-input').val();
     result.text('');
   
-    if (validateEmail(email)) result.text(email + ' is valid :)');
-    else result.text(email + ' is not valid :(');
+    if (validateEmail(email)) {
+        result.text('is valid :)');
+        success = true;
+    }
+    else {
+        result.text('is not valid :(');
+        success = false;
+    }
+    return success;
 }
 
 let prevWidth = $(".newsletter-button").width();
 let prevHeight = $(".newsletter-button").height();
 $(".newsletter-button").click( ()=> {
-    $(".newsletter-button").html('<div class="spin"><img class="h-100" src="icons/spin.svg"></div>');
-    $(".newsletter-button").css("background-color","black");
+    $(".newsletter-button").html('<div class="spin"><img class="h-100" src="icons/spin.svg"></div>').css("background-color","black");
     $(".spin").css({
         "width" : prevWidth,
         "height": prevHeight
     });
-    validate();
+    if(validate()) {
+        let result=$('.result').last();
+        result.removeClass("bottom").slideUp().css("text-align","right");
+        setTimeout(()=> {
+            $('.email-input').slideUp();
+            $('.email-text').css("color","transparent");
+            $('.email-shadow').slideUp();
+        }, 1000)
+        setTimeout(()=> {
+            result.slideDown();
+            $(".spin").children().attr("src","icons/check.svg").css({
+                "animation" : "none",
+                "width" : "40%"
+            });
+        }, 2000)
+
+    }
 })
 
-console.log($('.result').last());
+console.log($(".spin").children());
