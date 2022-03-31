5c7b55bca788b6ebe73f3f0f9686c561b5304c30
xjaszczomb <kovvalska@onet.pl>
2022-03-31 15:22:32 +0200

    Validate, animations function for email box

--- js/email.js
+++ js/email.js
@@ -1,55 +1,91 @@
+
+function currentWidth() {
+    return $(window).width();
+}
+
+function successButton() {
+    setTimeout(()=> {
+        currentMessagePlace().slideDown();
+        $(".spin").children().attr("src","icons/check.svg").css({
+            "animation" : "none",
+            "width" : "40%"
+        });
+    }, 2000)
+}
+function animationDesktop(status){
+    if(status) {
+        currentMessagePlace().removeClass("bottom").slideUp().css("text-align","right");
+        setTimeout(()=> {
+            $('.email-input').slideUp();
+            $('.email-shadow').slideUp();
+            $('.email-text').css("color","transparent");
+            currentMessagePlace().addClass("w-100").css("margin-block-end","0");
+        }, 1000)
+        successButton();
+    } else {
+        $('.result').first().text(resultFirstText);
+        setTimeout(()=> {
+            $(".newsletter-button").html("send").css("background-color","var(--greenhaze)");
+        }, 2000)
+    }
+}
+function animationMobile(status) {
+    if(status) {
+        $('.email-input').slideDown();
+        $('.email-shadow').slideDown();
+        $('.email-text').css("color","white");
+        currentMessagePlace().removeClass("w-100").css("margin-block-end","0.5em");
+        successButton();
+    }
+    else {
+        $('.result').last().text("");
+        setTimeout(()=> {
+            $(".newsletter-button").html("send").css("background-color","var(--greenhaze)");
+        }, 2000)
+    }
+}
+function currentAnimation(status) {
+    return currentWidth() < 992 ? animationMobile(status) : animationDesktop(status);
+}
 const validateEmail = (email) => {
     return email.match(
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
 };
-function currentWidth() {
-    return $(window).width();
+function currentMessagePlace() {
+    return currentWidth() < 992 ? $('.result').first() : $('.result').last();
 }
-function validate() {
-    let result, success;
-    if(currentWidth() < 992) result = $('.result').first();
-    else result = $('.result').last();
-
-    const email = $('.email-input').val();
-    result.text('');
-  
+function validate(email) {
+    let success;
+    currentMessagePlace().text('');
+    
     if (validateEmail(email)) {
-        result.text('is valid :)');
         success = true;
+        setTimeout(()=> {
+            currentMessagePlace().text('Thank you for joining our newsletter! :)');
+        },2000)
     }
     else {
-        result.text('is not valid :(');
+        setTimeout(()=> {
+            currentMessagePlace().text('Email is not valid, please try again.');
+        },2000)
         success = false;
     }
     return success;
 }
 
+const initSpin ='<div class="spin"><img class="h-100" src="icons/spin.svg"></div>';
 let prevWidth = $(".newsletter-button").width();
 let prevHeight = $(".newsletter-button").height();
+let resultFirstText = $('.result').first().text();
 $(".newsletter-button").click( ()=> {
-    $(".newsletter-button").html('<div class="spin"><img class="h-100" src="icons/spin.svg"></div>').css("background-color","black");
+    const email = $('.email-input').val();
+    $(".newsletter-button").html(initSpin).css("background-color", "black");
     $(".spin").css({
         "width" : prevWidth,
         "height": prevHeight
     });
-    if(validate()) {
-        let result=$('.result').last();
-        result.removeClass("bottom").slideUp().css("text-align","right");
-        setTimeout(()=> {
-            $('.email-input').slideUp();
-            $('.email-text').css("color","transparent");
-            $('.email-shadow').slideUp();
-        }, 1000)
-        setTimeout(()=> {
-            result.slideDown();
-            $(".spin").children().attr("src","icons/check.svg").css({
-                "animation" : "none",
-                "width" : "40%"
-            });
-        }, 2000)
-
-    }
+    currentAnimation(validate(email));
+    $(window).resize(()=> currentAnimation(validate(email)));
 })
 
-console.log($(".spin").children());
